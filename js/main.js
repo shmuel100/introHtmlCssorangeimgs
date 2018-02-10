cff40720d5fd21d74c1bcb1ae2ab21158542fd1b
April Martin-Brillo <bronteausten@sbcglobal.net>
2018-02-10 13:55:58 -0800

    upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var family = ["Ben", "April", "Jackson", "Auntie Katie", "Auntie Gigi"]
+
+console.log(family.length)
+
+for(var  i = 0; i < family.length; i++){
+//console.log(family[2])
+
+//console.log(family[0].length);
+
+console.log(family[i].length);
+}
+
+var numbers = [12, 8, 505, 220, 2, 33]
+
+console.log(numbers.sort())
